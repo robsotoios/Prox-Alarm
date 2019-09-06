c57023bc9e11d2c669828320e321eacfbcb73ecb
Rob <robsotoios@gmail.com>
2019-09-06 13:18:37 -0400

    Initial Commit

---
+++ Prox AlarmTests/Prox_AlarmTests.swift
@@ -0,0 +1,34 @@
+//
+//  Prox_AlarmTests.swift
+//  Prox AlarmTests
+//
+//  Created by Robert Soto on 9/6/19.
+//  Copyright © 2019 Adapptt. All rights reserved.
+//
+
+import XCTest
+@testable import Prox_Alarm
+
+class Prox_AlarmTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
